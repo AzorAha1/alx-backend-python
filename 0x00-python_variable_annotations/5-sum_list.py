456444fb30ca830a220c038ec6af9866533d2e95
AzorAha1 <wizfaiz@icloud.com>
2024-04-19 02:12:58 +0100

    5-sum_list.py

---
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""this is the comment"""
+
+
+from typing import List
+
+
+def sum_list(input_list: List[float]) -> float:
+    """sum list function"""
+    return sum(input_list)
