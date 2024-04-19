3affeef96c8512b36dda55cb9e24a540c274e7a2
AzorAha1 <wizfaiz@icloud.com>
2024-04-19 01:31:15 +0100

    2-floor.py

--- 0x00-python_variable_annotations/2-floor.py
+++ 0x00-python_variable_annotations/2-floor.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 """function type annotation"""
-import math
+
 
 
 def floor(n: float) -> float:
     """funtion to return floor"""
-    return math.floor(n)
+    return floor(n)
