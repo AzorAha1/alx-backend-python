1b90d1e8f851c598b6f72735ea206de0c170a016
AzorAha1 <wizfaiz@icloud.com>
2024-04-22 23:22:27 +0100

    4-tasks.py

--- 0x01-python_async_function/3-tasks.py
+++ 0x01-python_async_function/3-tasks.py
@@ -8,7 +8,6 @@ wait_random = __import__('0-basic_async_syntax').wait_random
 
 def task_wait_random(max_delay: int) -> asyncio.Task:
     """_summary_
-
     Args:
         max_delay (int): _description_
 
---
+++ 0x01-python_async_function/4-tasks.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+"""this is an async function"""
+import asyncio
+from typing import List
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+async def task_wait_n(n: int, max_delay: int) -> List[float]:
+    """_summary_
+    Args:
+        n (int): _description_
+        max_delay (int): _description_
+
+    Returns:
+        list[float]: _description_
+    """
+    listofdelays = [
+        asyncio.create_task(wait_random(max_delay)) for _ in range(n)
+        ]
+    results = await asyncio.gather(*listofdelays)
+    sortedresults = [result for result in sorted(results)]
+    return sortedresults
