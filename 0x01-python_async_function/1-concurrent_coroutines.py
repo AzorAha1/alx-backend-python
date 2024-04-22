3b11236613444f48f18b9e4ed1897cc10216cccb
AzorAha1 <wizfaiz@icloud.com>
2024-04-22 21:59:32 +0100

    1-concurrent_coroutines.py

---
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+"""sumary_line
+
+Keyword arguments:
+argument -- description
+Return: return_description
+"""
+
+
+import asyncio
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+async def wait_n(n: int, max_delay: int) -> list[float]:
+    """_summary_
+
+    Args:
+        n (int): _description_
+        max_delay (int): _description_
+
+    Returns:
+        list[float]: _description_
+    """
+    listofdelays = []
+    for _ in range(n):
+        listofdelays.append(wait_random(max_delay))
+    results = await asyncio.gather(*listofdelays)
+    return results
