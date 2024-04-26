b24ff91edc5d42e68785ff834c540aa9b560ec7f
AzorAha1 <wizfaiz@icloud.com>
2024-04-26 19:53:51 +0100

    0-async_generator.py

---
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""sumary_line"""
+
+
+import asyncio
+import random
+
+
+async def async_generator():
+    """sumary_line
+    Keyword arguments:
+    argument -- description
+    Return: return_description
+    """
+    for _ in range(10):
+        await asyncio.sleep(1)
+        therandom = random.uniform(0, 10)
+        yield therandom
