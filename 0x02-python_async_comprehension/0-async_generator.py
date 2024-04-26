e2ba2cd0bd92d54fa35100f7635a35f752d408b0
AzorAha1 <wizfaiz@icloud.com>
2024-04-26 20:21:52 +0100

    0-async_generator.py

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -7,7 +7,7 @@ import random
 from typing import Generator
 
 
-async def async_generator() -> Generator[float, None, None]: # type: ignore
+async def async_generator() -> Generator[float, None, None]:  # type: ignore
     """sumary_line
     Keyword arguments:
     argument -- description
