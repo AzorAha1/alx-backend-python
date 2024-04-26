3341efedf96495b1e01fdf64d16430e94f6f9b42
AzorAha1 <wizfaiz@icloud.com>
2024-04-26 20:17:00 +0100

    0-async_generator.py

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -4,13 +4,14 @@
 
 import asyncio
 import random
+from typing import AsyncGenerator
 
 
-async def async_generator():
+async def async_generator() -> AsyncGenerator[float, None, None]:
     """sumary_line
     Keyword arguments:
     argument -- description
-    Return: return_description
+    Return: return_descriptin
     """
     for _ in range(1, 11):
         await asyncio.sleep(1)
