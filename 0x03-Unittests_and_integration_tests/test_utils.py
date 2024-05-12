ecede0929c6d101d05f4c4e917d19783f808ed25
AzorAha1 <wizfaiz@icloud.com>
2024-05-12 20:57:28 +0100

    update

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -19,13 +19,11 @@ class TestAccessNestedMap(unittest.TestCase):
     Args:
         unittest (_type_): _description_
     """
-
     @parameterized.parameterized.expand([
         ({"a": 1}, ("a",), 1),
         ({"a": {"b": 2}}, ("a",), {"b": 2})
         ({"a": {"b": 2}}, ["a", "b"], 2),
     ])
-    def test_access_nested_map(self, nested_map: Mapping, path:Sequence, expected: Any) -> Any:
+    def test_access_nested_map(self, nested_map, path, expected) -> Any:
         """test access_nested_map function"""
-        result = access_nested_map(nested_map, path)
-        self.assertEqual(result, expected)
+        self.assertEqual(access_nested_map(nested_map, path), expected)
